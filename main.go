1aaae4c6c185848a878dcc115a527acc9436cca5
Ari Rubinstein <ari@agoric.com>
2022-10-21 18:01:57 -0700

    fix: add warning if date < proposal period

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ func main() {
 	fDate := flag.String("date", "", "specific time to estimate height at, e.g. Mon Jan 2 15:04:05 MST 2006")
 	fSamples := flag.Int64("samples", 100, "number of samples to take")
 	fRpc := flag.String("rpc", "https://main.rpc.agoric.net:443", "the rpc endpoint to sample from")
+	fVotingPeriod := flag.Duration("votingPeriod", 24*3*time.Hour, "the voting period of the destination chain")
 	fThreads := flag.Int64("threads", 6, "number of threads to use")
 	fStatmode := flag.String("statmode", "mean", "statmode: mean or median")
 	fTimezones := flag.String("timezones", "Local,UTC,Asia/Tokyo,Australia/NSW,Asia/Istanbul,US/Pacific,US/Eastern", "comma-separated list of timezones")
@@ -46,6 +47,12 @@ func main() {
 			fmt.Println(err.Error())
 			return
 		}
+		if time.Duration(estTime.UnixNano()-time.Now().UnixNano()) < *fVotingPeriod+time.Hour {
+			fmt.Println("****** WARNING ******")
+			fmt.Println("The Date above is before the voting period ends")
+			fmt.Println(time.Duration(estTime.UnixNano() - time.Now().UnixNano()).String())
+			fmt.Println("****** WARNING ******")
+		}
 
 		fmt.Printf("Estimated time for height %d:\n", *fHeight)
 		tzs := strings.Split(*fTimezones, ",")
@@ -69,11 +76,20 @@ func main() {
 			fmt.Println(err.Error())
 			return
 		}
+
 		estBlock, err := esti.CalcBlock(date)
 		if err != nil {
 			fmt.Println(err.Error())
 			return
 		}
+
+		if time.Duration(date.UnixNano()-time.Now().UnixNano()) < *fVotingPeriod+time.Hour {
+			fmt.Println("****** WARNING ******")
+			fmt.Println("The Date above is before the voting period ends")
+			fmt.Println(time.Duration(date.UnixNano() - time.Now().UnixNano()).String())
+			fmt.Println("****** WARNING ******")
+		}
+
 		fmt.Println("Estimated block height at", date.Format(time.UnixDate), "is", estBlock)
 	} else {
 		fmt.Println("Must provide either a -height or a -date to estimate")
